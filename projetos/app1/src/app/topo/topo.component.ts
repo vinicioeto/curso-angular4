dc1149c1964fb5aadbb75f65f2512250f7c8b994
vinicioeto <eto.vinicio@gmail.com>
2019-06-30 22:09:45 -0300

    criando componentes

---
+++ projetos/app1/src/app/topo/topo.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-topo',
+  // selector: '[app-topo]',
+  // selector: '.app-topo',
+  templateUrl: './topo.component.html',
+  // template: `
+  //   <p>
+  //     Esse é o componente topo</p>
+  //   `
+  // styles: [`.exemplo {
+  //   color: red
+  // }`]
+  styleUrls: ['./topo.component.css']
+})
+export class TopoComponent {}
